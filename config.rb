7f0569cf317b18c8271b2389ad4d62f468e12779
Adriano Fernandes da Silva <adrianof@lt-mbp-sp-022.ny.rga.com>
2014-06-04 09:51:06 -0300

    adicionando templates

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@
 
 set :css_dir   , 'public/stylesheets/'
 set :images_dir, 'public/images/'
-set :layout    , 'development_nike'
+set :layout    , 'development'
 
 # -----------------------------
 #  Activate items
@@ -33,7 +33,7 @@ end
 
 configure :build do
   activate :minify_emails
-  set      :layout     , "production_nike"
-  set      :http_prefix, "http://www.casafenomenal.com.br"
+  set      :layout     , "production"
+  set      :http_prefix, "url_assets"
   set      :images_dir , 'email/images/site/'
 end
